63380ccda03b2df1a3819dc8bd1789bbf2e33ebf
s0h3ck <s0h3ck@gmail.com>
2020-04-30 19:20:42 -0400

    Update test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,14 @@
-from unittest import TestCase
-from npmworkon import main
+import argparse
+from unittest import TestCase, mock
+from npmworkon import NpmWorkon
 
-class TestConsole(TestCase):
-    def test_main(self):
-        main() # TODO: 
+@mock.patch('argparse.ArgumentParser.parse_args',
+            return_value = argparse.Namespace(
+                environment = None,
+                version = None,
+                rm = None)
+            )
+def test_no_options(mock_args):
+    npmworkon = NpmWorkon()
+    res = npmworkon.run()
+    assert res == None # TODO:
